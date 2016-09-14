f28ba959c7365ae9c97547005b05ddd40b50dc9e
Isaac Rozas <irozgar@users.noreply.github.com>
2016-09-14 23:46:06 +0200

    More tasks related with using the gulp-rev-versions-bundle
    * Remove remaining service off the twig extenstion for assets
    
    * added test to ensure assets paths are replaced with the ones generated by gulp

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -15,4 +15,14 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testGulpGeneratedPathsAreUsedInView()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('js/scripts-07fb3d8168.js', $crawler->filterXPath('//body/script')->attr('src'));
+    }
 }
